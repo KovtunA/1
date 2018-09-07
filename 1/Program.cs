5c7598fd5b3375c8c16a143d8f03ded1e875aad7
KovtunA <42818685+KovtunA@users.noreply.github.com>
2018-09-07 16:31:06 +0300

    project with crash

---
+++ 1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter number A: ");
+                Int32 Number1 = getNumberOrExit();
+                Console.Write("Enter number B: ");
+                Int32 Number2 = getNumberOrExit();
+
+                if (Number1 % 2 == 0)
+                {
+                    Int32 Final = Number1 * Number2;
+                    Console.WriteLine("a*b={0}", Final);
+                }
+
+                else
+                {
+                    Int32 Final = Number1 + Number2;
+                    Console.WriteLine("a+b={0}", Final);
+                }
+            }
+        }
+
+        private static bool shouldExit(String userInput) {
+            return userInput == "q" || userInput == "quit" || userInput == "Q";
+        }
+
+        private static Int32 getNumberOrExit() {
+            string userInput = Console.ReadLine();
+            if (shouldExit(userInput)) { Environment.Exit(0); }
+
+            try { return Convert.ToInt32(userInput); }
+            catch
+            {
+                Console.WriteLine("Invalid input, please try again or 'q' to exit");
+                return getNumberOrExit();
+            }
+        }
+    }
+}
---
+++ 1/Program1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string user_types = (" ");
+            do
+            {
+                Int32 Number1;
+                Int32 Number2;
+                Int32 Final = 0;
+
+                Console.Write("Enter number A");
+                Number1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter number B");
+                Number2 = Convert.ToInt32(Console.ReadLine());
+
+                {
+                    if (Number1 % 2 == 0)
+                    {
+                        Final = Number1 * Number2;
+                        Console.WriteLine("a*b={0}", Final);
+                    }
+
+                    else
+                    {
+                        Final = Number1 + Number2;
+                        Console.WriteLine("a+b={0}", Final);
+
+                    }
+
+                }
+            }
+            while (user_types != "q");
+        }
+    }
+}
